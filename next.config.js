79e8b5d61e512b2d60d22dbef76e0f201d67d796
huzaifa-99 <huzaifarasheed900@gmail.com>
2022-07-04 01:24:32 +0500

    added html default language

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // NOTE: locales are also added in routing
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
 }
 
 module.exports = nextConfig
